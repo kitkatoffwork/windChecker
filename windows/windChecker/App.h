f77e089608e540596b4d1cf45645bcd010651cbb
SEITA <taha.seita@gmail.com>
2021-02-21 16:48:56 +0900

    first commit

---
+++ windows/windChecker/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::windChecker::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::windChecker::implementation
+
+
